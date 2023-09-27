346db91f893dd2412219c4db8921458c7246d924
tubuianh <anhtu2122002@gmail.com>
2023-09-27 21:26:08 +0700

    Buoi_1

---
+++ WebDevelop/WebDevelop/Areas/Areas/AreasAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebDevelop.Areas.Areas
+{
+    public class AreasAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Areas";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Areas_default",
+                "Areas/{controller}/{action}/{id}",
+                new {controller = "Dashboard", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebDevelop/WebDevelop/Areas/Areas/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDevelop.Areas.Areas.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Areas/Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
