e7244208bfcc8f7abe1e77aafd2b9ba91b9e592e
tubuianh <anhtu2122002@gmail.com>
2023-09-29 20:49:59 +0700

    Buoi2

---
+++ WebDevelop/MyClass/Model/Orderdetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyClass.Model
+{
+    [Table("Orderdetails")]
+    public class Orderdetails
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+        public int ProductID { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Qty { get; set; }
+
+    }
+}
--- WebDevelop/WebDevelop/Controllers/SiteController.cs
+++ WebDevelop/WebDevelop/Controllers/SiteController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyClass.Model;
 
 namespace WebDevelop.Controllers
 {
@@ -11,6 +12,9 @@ namespace WebDevelop.Controllers
         // GET: Site
         public ActionResult Index()
         {
+            MyDBContext db = new MyDBContext();
+            int sodong = db.Products.Count();
+            ViewBag.sodong = sodong;
             return View();
         }
     }
